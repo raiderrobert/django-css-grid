ce88380db5c3d967b66408d20fe105b1b3c7eb63
Robert Roskam <raiderrobert@gmail.com>
2017-09-13 22:40:53 -0400

    Create views.py

---
+++ css-grid/views.py
@@ -0,0 +1,21 @@
+"""
+CSS Grid Template
+"""
+
+class CSSGridMixin(object):
+    """
+    A mixin for adding css grid to any standard CBV
+    """
+
+    def get_context_data(self, **kwargs):
+        """
+        Insert the single object into the context dict.
+        """
+        context = {}
+        if self.object:
+            context['object'] = self.object
+            context_object_name = self.get_context_object_name(self.object)
+            if context_object_name:
+                context[context_object_name] = self.object
+        context.update(kwargs)
+        return super(SingleObjectMixin, self).get_context_data(**context)
