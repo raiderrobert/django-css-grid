96e2e16638f8e46d022edf30a7eadfdb897d1e0c
Robert Roskam <raiderrobert@gmail.com>
2017-09-22 16:03:06 -0400

    Update views.py

--- css-grid/views.py
+++ css-grid/views.py
@@ -6,16 +6,23 @@ class CSSGridMixin(object):
     """
     A mixin for adding css grid to any standard CBV
     """
+    
+    _prefix = 'css-grid'
+    grid_wrapper = None
+    grid_template_columns = None
+    grid_template_areas = None
+    grid_gap = None
+    
 
     def get_context_data(self, **kwargs):
         """
         Insert the single object into the context dict.
         """
-        context = {}
-        if self.object:
-            context['object'] = self.object
-            context_object_name = self.get_context_object_name(self.object)
-            if context_object_name:
-                context[context_object_name] = self.object
-        context.update(kwargs)
-        return super(SingleObjectMixin, self).get_context_data(**context)
+        context = super(SingleObjectMixin, self).get_context_data(**context)
+        context.update({
+            'grid_wrapper': self._prefix + self.grid_wrapper,
+            'grid-template-columns': self._prefix + self.grid_template_columns,
+            'grid-template-areas': self._prefix + self.grid_template_areas,
+            'grid-gap': self._prefix + self.grid_gap
+        })
+        return context
